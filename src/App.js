a276780ca991c9fb42c6b554a8c9d47968d824a9
vinukola <udaysekhar4@gmail.com>
2022-06-16 21:13:55 +0530

    code structure modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from "react";
-import "./style.css";
+import './style.css';
+import React, { Component } from 'react';
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello StackBlitz!</h1>
+        <p>Start editing to see some magic happen :)</p>
+      </div>
+    );
+  }
 }
+
+export default App;
